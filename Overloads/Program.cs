0806f2d61b9d2864fd793ddf1766989ed5668a14
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-06-14 14:07:07 +0200

    Add project files.

---
+++ Overloads/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Overloads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var welcome = new WelcomeMessage("Hi and welcome to the room :)");
+            var welcome2 = new WelcomeMessage("Hi and welcome", "you are nice");
+            Console.WriteLine($"{welcome.Hey}");
+            Console.WriteLine($"{welcome2.Hey}, {welcome2.Compliment}");
+
+        }
+    }
+}
---
+++ Overloads/WelcomeMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloads
+{
+    internal class WelcomeMessage
+    {
+        public string Hey;
+
+        public WelcomeMessage(string hey)
+        {
+            Hey = hey;
+        }
+
+        public string Compliment;
+        public WelcomeMessage(string hey, string compliment)
+        {
+            Hey = hey;
+            Compliment = compliment;
+        }
+    }
+}
